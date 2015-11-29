906b597997493c5f90a8e0257a49e312bcefc44a
haruosuz <haruosuz@yahoo.co.jp>
2015-11-29 22:13:10 +0900

    update README.md scripts/

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 
 mkdir data analysis
 
+# Creating some empty README files in data/
+
+touch data/README
 
 # Running R scripts
 
 Rscript --vanilla scripts/my_analysis.R > analysis/output.txt
-
